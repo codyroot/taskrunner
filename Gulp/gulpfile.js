66bc1f61679da327a426f5daaa2e54880f88785a
codyroot <smithyyy@myopera.com>
2014-05-01 21:09:28 +0200

    init commit

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -1,66 +1,109 @@
 var gulp = require("gulp");
-var preprocess = require('gulp-preprocess');
-var spritesmith = require('gulp.spritesmith');
-var connect = require('gulp-connect');
+var preprocess = require("gulp-preprocess");
+var spritesmith = require("gulp.spritesmith");
+var connect = require("gulp-connect");
 var less = require("gulp-less");
+var path = {
+    css: "./style/",
+    less: "./style/",
+    html: {
+        root: "",
+        src: "./tmp/",
+        dest: "./"
+    },
+    sprite: {
+        src: "./img/all/*.png",
+        dest: "./img",
+        name: "sprite-",
+        imgLocation: "../img/",
+        cssFormat: "css"
+    }
+};
 
-// WebServer
-gulp.task('webserver', function () {
+/* 
+ ******************************************************************************* 
+ * WebServer
+ ******************************************************************************* 
+ */
+gulp.task("webserver", function () {
     connect.server({
-        livereload: true
+        livereload: true,
+        //root: "",
+        port: 8080//,
+        //host: "dev"
     });
 });
 
-gulp.task('reload', function () {
-    gulp.src('home.html ')
-        .pipe(connect.reload());
+/*
+ ******************************************************************************* 
+ * Static HTML Builder + Reload bei index.html Änderung
+ ******************************************************************************* 
+ */
+
+// Builder
+gulp.task("html", function () {
+    gulp.src(path.html.src + "*.html")
+        .pipe(preprocess())
+        .pipe(gulp.dest(path.html.dest));
 });
 
-gulp.task('reload2', function () {
-    gulp.src('style/*.css')
-        .pipe(connect.reload());
+// Reload
+gulp.task("reload", function () {
+    gulp.src(path.html.root + "*.html").pipe(connect.reload());
 });
 
-gulp.task('watchMe', function () {
-    gulp.watch('home.html', ['reload']);
-	gulp.watch('style/*.css', ['reload2']);
+/* 
+ ******************************************************************************* 
+ * LESS Compiler
+ ******************************************************************************* 
+ */
+gulp.task("less", function() {
+  gulp.src(path.less + "/style.less")
+    .pipe(less({
+        sourceMap: true
+    }))
+    .pipe(gulp.dest(path.css))
 });
 
-gulp.task('default', ['webserver', "watchMe"]);
-
-
-gulp.task('less', function() {
-  gulp.src('style/style.less')
-    .pipe(less())
-    .pipe(gulp.dest('style'))
-    .pipe(connect.reload());
-});
-
-// PNG Sprite Generator
-gulp.task('sprite', function () {
+/* 
+ ******************************************************************************* 
+ * PNG Sprite Generator
+ ******************************************************************************* 
+ */
+gulp.task("sprite", function () {
     var mySprite = gulp
-        .src('./images/all/*.png')
+        .src(path.sprite.src)
         .pipe(spritesmith({
-            imgName: 'sprite.png',
-            cssName: 'sprite.css',
-            cssFormat: "css", // less, sass
+            imgName: "sprite.png",
+            cssName: "sprite.css",
+            cssFormat: path.sprite.cssFormat, // less, sass
             cssVarMap: function (sprite) {
                 // sprite. --> name, image(pfad), x, y, width, height, total_width, total_height
-                sprite.name = 'sprite-' + sprite.name.replace(/\s/g, "");
-                sprite.image = "../images/" + sprite.image;
+                sprite.name = path.sprite.name + sprite.name.replace(/\s/g, "");
+                sprite.image = path.sprite.imgLocation + sprite.image;
             }
         }));
-    mySprite.img.pipe(gulp.dest('./images'));
-    mySprite.css.pipe(gulp.dest('./css'));
+    mySprite.img.pipe(gulp.dest(path.sprite.dest));
+    mySprite.css.pipe(gulp.dest(path.css));
 });
 
-// Static HTML Builder
-gulp.task('html', function () {
-    gulp.src('./snips/*.html')
-        .pipe(preprocess())
-        .pipe(gulp.dest('./'))
-});
-
+/* 
+ ******************************************************************************* 
+ * Watcher
+ ******************************************************************************* 
+ */
 gulp.task("watch", function () {
-    gulp.watch("./snips/**/*.html", ["html"]);
+    gulp.watch("./tmp/**/*.html", ["html"]);
+    gulp.watch(path.less + "*.less", ["less"]);
+    gulp.watch("index.html", ["reload"]);
+    gulp.watch(path.css + "*.css", ["reload"]);
 });
+
+/* 
+ ******************************************************************************* 
+ * Tasks
+ ******************************************************************************* 
+ */
+gulp.task("default", ["webserver", "watch"]);
+
+gulp.task("image", ["sprite"]);
